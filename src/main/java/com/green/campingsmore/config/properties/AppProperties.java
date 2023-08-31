9696e24e32cc70044342d4c9a7b6cd0fbf4c8052
황주은 <rlahfld54@naver.com>
2023-08-31 16:05:35 +0900

    추가 설정 및 세팅 에러 - 황주은

---
+++ src/main/java/com/green/campingsmore/config/properties/AppProperties.java
@@ -0,0 +1,56 @@
+package com.green.campingsmore.config.properties;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import java.security.Key;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@ConfigurationProperties(prefix = "app")
+public class AppProperties {
+    private final Auth auth = new Auth();
+    private final OAuth2 oauth2 = new OAuth2();
+
+    @Setter
+    public Key accessTokenKey;
+
+    @Setter
+    private Key refreshTokenKey;
+
+    @Getter
+    @Setter
+    public static class Auth {
+        private String headerSchemeName;
+        private String tokenType;
+        private String aceessSecret;
+        private long accessTokenExpiry;
+        private String refreshSecret;
+        private long refreshTokenExpiry;
+        private String redisAccessBlackKey;
+        private String redisRefreshKey;
+    }
+
+    @Getter
+    @Setter
+    public static final class OAuth2 {
+        private List<String> authorizedRedirectUris = new ArrayList<>();
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/main/java/com/green/campingsmore/config/utils/MyHeaderUtils.java
@@ -0,0 +1,75 @@
+package com.green.campingsmore.config.utils;
+import com.green.campingsmore.config.properties.AppProperties;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.util.SerializationUtils;
+
+import java.util.Base64;
+import java.util.Optional;
+
+@Component
+@RequiredArgsConstructor
+public class MyHeaderUtils {
+    private final AppProperties APP_PROPERTIES;
+
+    public String getAccessToken(HttpServletRequest req) {
+        String headerValue = req.getHeader(APP_PROPERTIES.getAuth().getHeaderSchemeName());
+        String tokenType = APP_PROPERTIES.getAuth().getTokenType();
+        return headerValue != null && headerValue.startsWith(tokenType)
+                ? headerValue.substring(tokenType.length()).trim()
+                : null;
+    }
+
+    public static Optional<Cookie> getCookie(HttpServletRequest request, String name) {
+        Cookie[] cookies = request.getCookies();
+
+        if (cookies != null && cookies.length > 0) {
+            for (Cookie cookie : cookies) {
+                if (name.equals(cookie.getName())) {
+                    return Optional.of(cookie);
+                }
+            }
+        }
+        return Optional.empty();
+    }
+
+    public static void addCookie(HttpServletResponse response, String name, String value, int maxAge) {
+        Cookie cookie = new Cookie(name, value);
+        cookie.setPath("/");
+        cookie.setHttpOnly(true);
+        cookie.setMaxAge(maxAge);
+
+        response.addCookie(cookie);
+    }
+
+    public static void deleteCookie(HttpServletRequest request, HttpServletResponse response, String name) {
+        Cookie[] cookies = request.getCookies();
+
+        if (cookies != null && cookies.length > 0) {
+            for (Cookie cookie : cookies) {
+                if (name.equals(cookie.getName())) {
+                    cookie.setValue("");
+                    cookie.setPath("/");
+                    cookie.setMaxAge(0);
+                    response.addCookie(cookie);
+                }
+            }
+        }
+    }
+
+    public static String serialize(Object obj) {
+        return Base64.getUrlEncoder()
+                .encodeToString(SerializationUtils.serialize(obj));
+    }
+
+    public static <T> T deserialize(Cookie cookie, Class<T> cls) {
+        return cls.cast(
+                SerializationUtils.deserialize(
+                        Base64.getUrlDecoder().decode(cookie.getValue())
+                )
+        );
+    }
+}
