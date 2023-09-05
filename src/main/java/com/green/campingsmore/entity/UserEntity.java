e8ae38e569fd1bd5738743a8c2880c5190ccd947
JeongHoBBang <dlawjdghbn@naver.com>
2023-09-05 17:09:37 +0900

    캠핑 상세 다중 사진 업로드-임정호

--- src/main/java/com/green/campingsmore/entity/UserEntity.java
+++ src/main/java/com/green/campingsmore/entity/UserEntity.java
@@ -56,53 +56,13 @@ public class UserEntity extends BaseEntity {
     @ColumnDefault("1")
     @JsonIgnore
     private int delyn;
-    //    @Column(name = "wishlist_array") // null true는 기본값이라서 설정안해줘도 된다.
-//    private String wishlistArray;
     @Column(name = "provider_type", length = 20,nullable = false)
     @Enumerated(EnumType.STRING)
+    @ColumnDefault("KAKAO")
     private ProviderType providerType;
     @JsonIgnore
     @Column(name = "role_type", length = 20,nullable = false)
     @Enumerated(EnumType.STRING)
+    @ColumnDefault("USER")
     private RoleType roleType;
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY) // auto_increment 설정
-//    @Column(nullable = false, updatable = false, columnDefinition = "BIGINT UNSIGNED", length = 15)
-//    private Long iuser;
-//    @Column(name = "user_id",updatable = false,nullable = false, length = 20)
-//    private String uid;
-//    @Column(nullable = false) // length 기본값 세팅 255임
-//    private String password;
-//    @Column(nullable = false,length = 100)
-//    @Size(min = 10, max = 50)
-//    private String email;
-//    @Column(nullable = false,length = 20)
-//    private String name;
-//    @Column(name = "birth_date",nullable = false,length = 10)
-//    private String birthDate;
-//    @Column(nullable = false,length = 11)
-//    private String phone;
-//    @Column(nullable = false,columnDefinition = "TINYINT not null CHECK(gender in (0,1))",length = 1)
-//    private Integer gender;
-//    @Column(name = "user_address",nullable = false,length = 100)
-//    private String userAddress;
-//    @Column(name = "user_address_detail",length = 100)
-//    private String userAddressDetail;
-//    @Column(nullable = false,length = 10)
-//    @ColumnDefault("'ROLE_USER'")
-//    private String role;
-//    private String pic;
-//    @Column(name = "del_yn",length = 1, columnDefinition = "TINYINT",nullable = false)
-//    @ColumnDefault("1")
-//    @JsonIgnore
-//    private int delyn;
-//    //    @Column(name = "wishlist_array") // null true는 기본값이라서 설정안해줘도 된다.
-////    private String wishlistArray;
-//    @Column(name = "provider_type", length = 20)
-//    @Enumerated(EnumType.STRING)
-//    private ProviderType providerType;
-//    @JsonIgnore
-//    @Column(name = "role_type", length = 20)
-//    @Enumerated(EnumType.STRING)
-//    private RoleType roleType;
 }
