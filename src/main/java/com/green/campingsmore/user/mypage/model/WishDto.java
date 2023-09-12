fa75ec5b754cf549b102a7aa730edd5debcd740a
황주은 <rlahfld54@naver.com>
2023-09-12 17:12:58 +0900

    찜하기 토글 기능 쿼리 추가 - 황주은

--- src/main/java/com/green/campingsmore/user/mypage/model/WishDto.java
+++ src/main/java/com/green/campingsmore/user/mypage/model/WishDto.java
@@ -4,9 +4,8 @@ import lombok.Data;
 
 @Data
 public class WishDto {
-    private int iwish;
-    private int iuser;
-    private int iitem;
-    private String created_at;
-    private int del_yn;
+    private Long iwish;
+    private Long iuser;
+    private Long iitem;
+    private Integer delYn;
 }
